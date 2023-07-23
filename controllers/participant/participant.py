c3f43c8c105a75d13813a1f43f06092a01cee23f
Pratyush <105967949+thePratyush07@users.noreply.github.com>
2023-07-23 16:08:49 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -16,12 +16,23 @@
    Demonstrates how to play a simple motion file."""
 
 from controller import Robot
+from controller import Camera
+from PIL import Image
+import numpy as np
+
 import sys
 
 # We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
 # If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
 from utils.motion_library import MotionLibrary
+
+robot = Robot()
+
+# Assuming the name of the bottom camera is 'bottom_camera'
+bottom_camera = robot.getCamera('CameraBottom')
+bottom_camera.enable(10)  # Enable the camera with a sampling period of 10 milliseconds (you can adjust the value)
+
 
 
 class Wrestler (Robot):
@@ -30,6 +41,16 @@ class Wrestler (Robot):
         motion_library = MotionLibrary()
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
         time_step = int(self.getBasicTimeStep())
+       while robot.step() != -1:
+    # Access the image data from the bottom camera
+    image_data = bottom_camera.getImage()
+
+    # Process the image data here (e.g., save the image, do image processing, etc.)
+
+    # Optional: Break the loop when a certain condition is met
+    # if some_condition:
+    #     break
+
         while self.step(time_step) != -1:  # mandatory function to make the simulation run
             motion_library.play('TaiChi')
 
